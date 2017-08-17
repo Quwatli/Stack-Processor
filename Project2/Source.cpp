77b7de1a577a9301f8e9052a325136832f16e7c4
Abdul Rahman Quwatli <abmaneastrum@gmail.com>
2017-08-17 12:47:55 -0400

    Visual Studio 17 retargetted and cleaned solution

---
+++ Project2/Source.cpp
@@ -0,0 +1,188 @@
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+class Stack
+{
+public:
+	Stack(int);
+	void push(int);
+	void pop();
+	int top();
+	bool isEmpty();
+	bool isFull();
+
+	//function to access the maxsize
+	int rtrnMaxSiz(){ return maxSize; }
+
+	//function to seasily traverse the array
+	int traverseStack(int i)
+	{
+		return stack[i];
+	}
+
+	//function to easily push in the middle
+	void pushMiddle(int x, int len)
+	{
+		stack[len / 2] = x;
+	}
+
+private:
+	int maxSize, *stack, stackTop;
+};
+
+Stack::Stack(int size)
+{
+	if (size <= 0)
+	{
+		cout << "Wrong size\n";
+		maxSize = 10;
+	}
+	else
+		maxSize = size;
+
+	stackTop = 0;
+	stack = new int[maxSize];
+}
+
+bool Stack::isEmpty()
+{
+	return (stackTop == 0);
+}
+
+bool Stack::isFull()
+{
+	return (stackTop == maxSize);
+}
+
+void Stack::push(int item)
+{
+	if (isFull())
+		cout << "Full stack" << endl;
+	else
+		stack[stackTop++] = item;
+}
+
+int Stack::top()
+{
+	assert(!isEmpty());
+
+	return stack[stackTop - 1];
+}
+
+void Stack::pop()
+{
+	if (isEmpty())
+		cout << "Empty stack" << endl;
+	else
+		stackTop--;
+}
+
+//copying from s1 to s2
+void copyStack(Stack s1, Stack s2)
+{
+	Stack Temp(10);
+	while (!s1.isEmpty())
+	{
+		Temp.push(s1.top());
+		s1.pop();
+	}
+
+	while (!Temp.isEmpty())
+	{
+		s2.push(Temp.top());
+		s1.push(Temp.top());
+		Temp.pop();
+	}
+}
+
+//finding greater values than x in stack s1
+void countGreater(int x, Stack s1)
+{
+	int count = 0;
+	for (int i = 0; i < s1.rtrnMaxSiz(); i++)
+	if (s1.traverseStack(i)>x)
+		count++;
+	cout << count << " elements greater than " << x << endl;
+}
+
+//finding the max value in stack s1
+void findMax(Stack s1)
+{
+	int max=0;
+	for (int i = 0; i < s1.rtrnMaxSiz(); i++)
+	if (s1.traverseStack(i)>max)
+		max = s1.traverseStack(i);
+	cout << "The maximum value in your stack is " << max << endl;
+}
+
+//inserting in the middle of stack s1
+void insertMiddle(int y, Stack s1)
+{
+	//calculating the lenght
+	int length = 0;
+	for (int i = 0; i <= s1.rtrnMaxSiz(); i++)
+	if (s1.traverseStack(i)>0)
+		length++;
+	s1.pushMiddle(y, length);
+
+}
+
+void main()
+{
+
+	/*Test Code
+	int main()
+	{
+		cout << "Write your main() function to test ALL functions. "
+			<< "We will run yuur code using YOUR AND OUR main()." << endl;
+		return 0;
+	}
+	*/
+
+	Stack s1(10), s2(10);
+	int z, x;
+	cout << "\nFill in stack with 10 integer values:" << endl;
+	for (int i = 0; i < 10; i++)
+	{
+		cin >> z;
+		s1.push(z);
+	}
+
+	cout << "\nStack s1 before copying s2:" << endl;
+	for (int i = 0; i < s1.rtrnMaxSiz(); i++)
+		cout << s1.traverseStack(i) << " ";
+	cout << endl;
+
+	copyStack(s1, s2);
+
+	cout << "\nStack s2 after copy:" << endl;
+	for (int i = 0; i < s1.rtrnMaxSiz(); i++)
+		cout << s2.traverseStack(i) << " ";
+	cout << endl;
+
+	cout << "\nStack s1 after copying s2:" << endl;
+	for (int i = 0; i < s1.rtrnMaxSiz(); i++)
+		cout << s1.traverseStack(i) << " ";
+	cout << endl;
+
+	cout << "\nEnter an integer to search for greater values in stack s1:" << endl;
+	cin >> x;
+	countGreater(x, s1);
+
+	findMax(s1);
+
+	cout << "\nEnter an integer to insert in the middle of the stack s1:" << endl;
+	cin >> x;
+	insertMiddle(x, s1);
+
+	for (int i = 0; i < s1.rtrnMaxSiz(); i++)
+	{
+		cout << s1.traverseStack(i) << " ";
+	}
+	cout << endl;
+
+	char c;
+	cout<<"\nWritten by A. Z. Quwatli\n" << "\nenter any key to continue";
+	cin >> c;
+}
